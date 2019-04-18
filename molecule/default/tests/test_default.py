1e8638ae641f23995d9f87d901bafb95fcdd5440
daniel-wtd <daniel@while-true-do.io>
2019-04-18 13:23:55 +0200

    fix: test missing

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,3 +4,8 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_ansible_package(host):
+    pkg = host.package("ansible")
+    assert pkg.is_installed
